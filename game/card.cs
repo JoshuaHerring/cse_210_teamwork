66d2e7583224631a7f7dc7dcf943a79cdd5a002e
Joshua Herring <jdherring2222@gmail.com>
2022-10-04 15:46:32 -0600

    made generateCard function

--- game/card.cs
+++ game/card.cs
@@ -4,15 +4,23 @@ namespace Test
 {
     public class Card
     {
-            public static int pickCard()
+        ///<summary>
+        ///<para>Choose a card to display to the player</para>
+        ///Returns an int between 1 and 13
+        ///</summary>
+        public static int generateCard()
             {
-                //choose a card to display to the player
-                return 1;
+                Random rnd = new Random();
+                int number = rnd.Next(1, 14);
+
+                return number;
             }
 
+            ///<summary>
+            ///Calculates the score based off of the card and the players guess
+            ///</summary>
             public static int determineScore(int guess, int card)
             {
-                //calculates the score based off of the card and the players guess
 
                 return 1;
             }
