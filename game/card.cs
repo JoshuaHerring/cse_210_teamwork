827d26cd23cee64667465cee4e7235cc6d7a3840
Michael Dunn <Dunnm2001@gmail.com>
2022-10-06 15:58:57 -0600

    Finished Game

--- game/card.cs
+++ game/card.cs
@@ -12,12 +12,14 @@ namespace Test
             {
                 Random rnd = new Random();
                 int number = rnd.Next(1, 14);
+                Console.WriteLine($"The card is {number}\n");
 
                 return number;
             }
 
             ///<summary>
             ///Calculates the score based off of the card and the players guess
+            /// return score
             ///</summary>
             public static int determineScore(string guess, int card1, int card2, int score)
             {
@@ -25,14 +27,17 @@ namespace Test
                 if (card1 < card2 && guess == "h")
                 {
                     score += 100;
+                    Console.WriteLine($"You guessed right! Your new score is {score}\n");
                 }
                 else if (card1 > card2 && guess == "l")
                 {
                     score += 100;
+                    Console.WriteLine($"You guessed right! Your new score is {score}\n");
                 }
                 else
                 {
                     score -= 75;
+                    Console.WriteLine($"You guessed wrong! Your new score is {score}\n");
                 }
                 return score;
             }
--- game/director.cs
+++ game/director.cs
@@ -8,9 +8,31 @@ namespace Test
         ///Asks the player if they would like to start the game, then runs the game
         ///<para>This will run the entire game</para>
         ///</summary>
-        public static string start()
+        public static void start()
         {
-            return "";
+            bool onward = true;
+            int score = 300;
+            Console.WriteLine($"Your starting score is {score}\n");
+            do
+            {
+            int card1 = Card.generateCard();
+            string guess = Director.guesses();
+            int card2 = Card.generateCard();
+            score = Card.determineScore(guess, card1, card2, score);
+            if(score <= 0)
+            {
+                onward = false;
+                break;
+            }
+            else
+                onward = proceed();
+            }
+            while(onward == true);
+            if(onward == false)
+            {
+                Console.WriteLine($"That is the end of the game!");
+            }
+
         }
         ///<summary>
         ///Asks the player what their guess is, then returns the string
@@ -18,13 +40,18 @@ namespace Test
         public static string guesses()
         {
             string guess;
-            Console.WriteLine("What is your guess? ('H,L') : ");
+            
+            Console.Write("What is your guess? ('H,L') : ");
             guess = Console.ReadLine().ToLower();
-            while(guess != "h" || guess != "l")
+            Console.WriteLine();
+
+
+            while(guess != "h" && guess != "l")
             {
                 Console.Write("You typed an invalid guess, please try again ('H,L') : ");
                 guess = Console.ReadLine().ToLower();
             }
+
             return guess;
         }
         ///<summary>
@@ -34,12 +61,20 @@ namespace Test
         {
             Console.Write("Would you like to continue? (y/n) : ");
             string stringContinue = Console.ReadLine().ToLower();
-            if(stringContinue == "t")
+            bool continueGame = false;
+            while (stringContinue != "y" && stringContinue != "n")
             {
-                
+            Console.Write("Incorrect input, try again (y/n) : ");
+            stringContinue = Console.ReadLine().ToLower();
+            Console.WriteLine();
             }
-
-            return true;
+            if(stringContinue == "y")
+            {
+                continueGame = true;
+            }
+            else
+                continueGame = false;
+            return continueGame;
         }
 
     }
