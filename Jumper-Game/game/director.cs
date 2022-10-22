b80437d568b4b2c3a018bf86b03413b8147c9ba9
Joshua Herring <jdherring2222@gmail.com>
2022-10-21 21:55:49 -0600

    finished jumper and started director startgame

--- Jumper-Game/game/director.cs
+++ Jumper-Game/game/director.cs
@@ -1,14 +1,26 @@
 // Josh is doing this
 
 using System;
+using System.Collections.Generic;
 
 namespace Test
 {
     public class director
     {
+            word word = new word();
+            jumper jumper = new jumper();
         public void startGame()
         {
+            List<string> paratrooper = jumper.originalImageGet();
+            int lengthOfImage = paratrooper.Count;
+            for (int y = 0; y < lengthOfImage; y++)
+                {
+                Console.WriteLine(paratrooper[y]);
+                }
+            Console.WriteLine();
 
+            string chosenWord = word.chosenWord;
+            List<string> letterList = word.letterListCreation(chosenWord);
         }
 
 
