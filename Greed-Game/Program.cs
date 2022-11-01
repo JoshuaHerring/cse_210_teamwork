ce8c1c3b703fbdbafb17b24a86f4f87382610aa5
Joshua Herring <jdherring2222@gmail.com>
2022-11-01 12:06:02 -0600

    understood raylib basics

--- Greed-Game/Program.cs
+++ Greed-Game/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-
+using Raylib_cs;
 namespace Greed_Game
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            
+             Raylib.InitWindow(800, 600, "Greed");
+
+             while (!Raylib.WindowShouldClose())
+             {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.RAYWHITE);
+                Raylib.DrawText("Hello World", 100, 250, 20, Color.GOLD);
+
+
+
+                Raylib.EndDrawing();
+             }
+             Raylib.CloseWindow();
         }
     }
 }
