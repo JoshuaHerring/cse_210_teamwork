925d42a95663bb07c6b188d92c8527521768116e
Joshua Herring <jdherring2222@gmail.com>
2022-11-03 14:10:41 -0600

    made score class (score up, score down, game over)

--- Greed-Game/Game/score.cs
+++ Greed-Game/Game/score.cs
@@ -5,6 +5,33 @@ namespace Greed_Game
 {
     public class Score
     {
-        
+        ///<summary> Increments score </summary>
+        public int pointGained(int score)
+        {
+            score++;
+            return score;
+        }
+
+        /// <summary> decrements score </summary>
+        public int pointLost(int score)
+        {
+            score--;
+            return score;
+        }
+
+        /// <summary> Input score to determine if game is over
+        ///<para> returns bool true if game is over </para>
+        /// </summary>
+        public bool gameOver(int score)
+        {
+            bool gameOver = false;
+            if (score <=0)
+            {
+                gameOver = true;
+            }
+            return gameOver;
+        }
+
+
     }
 }
