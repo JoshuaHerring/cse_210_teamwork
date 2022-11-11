2beca5fdbcd6274d61c25acc4918ffaf72caa3a1
Joshua Herring <jdherring2222@gmail.com>
2022-11-10 20:26:30 -0700

    changed rows to coords

--- Greed-Game/Game/movementFiles/fallingObject.cs
+++ Greed-Game/Game/movementFiles/fallingObject.cs
@@ -8,24 +8,27 @@ namespace Greed_Game
 {
     public class FallingObjects : Movement
     {
-        private List<int> generateRow()
+        TerminalServices terminalServices = new TerminalServices();
+        TerminalServices.coords coords = new TerminalServices.coords();
+        private List<TerminalServices.coords> generateRow()
         {
-            List<int> row = new List<int>();
+            List<TerminalServices.coords> row = new List<TerminalServices.coords>();
             for (int i = 0; i > Raylib.GetRandomValue(3, 12); i++)
             {
-            row.Add(Raylib.GetRandomValue(0, 1000));
-            row.Add(0);
+                coords.x = Raylib.GetRandomValue(0, 1000);
+                coords.y = 0;
+            row.Add(coords);
             }
 
             return row;
         }
 
-        public List<List<int>> generateRows()
+        public List<List<TerminalServices.coords>> generateRows()
         {
-            List<List<int>> rows = new List<List<int>>();
+            List<List<TerminalServices.coords>> rows = new List<List<TerminalServices.coords>>();
             for (int i = 0; i > 4; i++)
             {
-                List<int> row = generateRow();
+                List<TerminalServices.coords> row = generateRow();
                 rows.Add(row);
 
             }
